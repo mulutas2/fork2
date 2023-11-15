f9a014aaec86b19199a18ababc44c52716db4e11
GenXu1 <42954360+GenXu1@users.noreply.github.com>
2023-11-15 10:33:13 -0500

    Add files via upload

---
+++ 04Kin_PCA/kin_pca.sh
@@ -0,0 +1,2 @@
+GEMMA -bfile ../02Genotype/278Sweet_corn_V5.miss05.maf001 -gk 1 -o 278Sweet_corn_V5.miss05.maf001
+plink --bfile ../02Genotype/278Sweet_corn_V5.miss05.maf001 --pca 278 --out 278Sweet_corn_V5.miss05.maf001
